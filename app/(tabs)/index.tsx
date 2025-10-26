c608824a01b3e8cd5989544c69eeba0674fb1d44
sun-ji054 <seonji04203645@gmail.com>
2025-10-26 17:04:04 +0900

    Feat: 둥근 버튼 구현 버튼 클릭했을 때 스타일 수정 필요

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,13 @@
-import { Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return <Text>초기 세팅</Text>;
+  return (
+    <View>
+        <Text>초기 세팅</Text>
+        <CustomButton label="멈춤"/>
+        <CustomButton label="멈춤" color="brown"/>
+        <CustomButton label="이어하기" size = "medium"/>
+    </View>
+  );
 }
---
+++ components/CustomButton.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
+
+interface CustomButtonProps {
+  label: string;
+  size?: "medium" | "small";
+  color?: "white" | "brown"
+}
+
+function CustomButton({label, size = "small", color = "white"}: CustomButtonProps) {
+  const textVariantStyle = color === 'brown' 
+    ? styles.textWhite
+    : styles.textBlack;
+  
+  return (
+    <Pressable style={({pressed}) => [styles.container, styles[size], styles[color], pressed && styles.pressed,]}>
+      <Text style={[styles.text, textVariantStyle]}>{label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  small: {
+    width: 130,
+    height: 60
+  },
+  medium: {
+    width: 160,
+    height: 60
+  },
+  white: {
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  brown: {
+    backgroundColor: "#976A50",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 500,
+    color: '#000000',
+    letterSpacing: 0.4,
+    lineHeight: 20,
+  },
+  textBlack: {
+    color: '#000000',
+  },
+  textWhite: {
+    color: '#FFFFFF',
+  },
+  pressed: {
+    backgroundColor: 'pink'
+  }
+});
+
+export default CustomButton;
