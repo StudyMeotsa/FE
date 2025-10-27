129f8b4849dc93d5ff97acf6b415057e0e754828
sun-ji054 <seonji04203645@gmail.com>
2025-10-27 15:02:34 +0900

    Feat: Submit 버튼 구현 userThumbnail 스크롤 되게 수정 필요

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,9 @@
-import CustomButton from "@/components/CustomButton";
-import SubmitButton from "@/components/SubmitButton";
-import UserThumbnail from "@/components/UserThumbnail";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View>
         <Text>초기 세팅</Text>
-        <CustomButton label="멈춤"/>
-        <CustomButton label="멈춤" color="brown"/>
-        <CustomButton label="이어하기" size = "medium"/>
-        <CustomButton label="스터디룸 만들기" size = "large" color="darkBrown"/>
-        <SubmitButton label="과제 1 내기"/>
-        <UserThumbnail />
     </View>
   );
 }
